bf57f5a6f1ab3bc29d5936c90c1f7a7fdea9c139
Lokeshkumar-9 <mloki0999@gmail.com>
2023-08-03 22:48:25 +0530

    first commit

--- game.py
+++ game.py
@@ -1,265 +1,87 @@
-from ps3 import *
-
-#
-# Test code
-#
-
-def test_get_word_score():
-    """
-    Unit test for get_word_score
-    """
-    failure=False
-    # dictionary of words and scores
-    words = {("", 7):0, ("it", 7):2, ("was", 7):54, ("weed", 6):176,
-             ("scored", 7):351, ("WaYbILl", 7):735, ("Outgnaw", 7):539,
-             ("fork", 7):209, ("FORK", 4):308}
-    for (word, n) in words.keys():
-        score = get_word_score(word, n)
-        if score != words[(word, n)]:
-            print("FAILURE: test_get_word_score()")
-            print("\tExpected", words[(word, n)], "points but got '" + \
-                  str(score) + "' for word '" + word + "', n=" + str(n))
-            failure=True
-    if not failure:
-        print("SUCCESS: test_get_word_score()")
-
-# end of test_get_word_score
+from ps3 import SCRABBLE_LETTER_VALUES
+import random
+from test_ps3 import test_is_valid_word
+from test_ps3 import test_get_word_score
+from test_ps3 import test_update_hand
+from test_ps3 import test_wildcard
 
 
-def test_update_hand():
-    """
-    Unit test for update_hand
-    """
-    # test 1
-    handOrig = {'a':1, 'q':1, 'l':2, 'm':1, 'u':1, 'i':1}
-    handCopy = handOrig.copy()
-    word = "quail"
+# Constants
+HAND_SIZE = 7
+SCRABBLE_LETTER_VALUES = {
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
+    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
+    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+}
 
-    hand2 = update_hand(handCopy, word)
-    expected_hand1 = {'l':1, 'm':1}
-    expected_hand2 = {'a':0, 'q':0, 'l':1, 'm':1, 'u':0, 'i':0}
-    if hand2 != expected_hand1 and hand2 != expected_hand2:
-        print("FAILURE: test_update_hand('"+ word +"', " + str(handOrig) + ")")
-        print("\tReturned: ", hand2, "\n\t-- but expected:", expected_hand1, "or", expected_hand2)
+# Function to load words from the words.txt file
+def load_words():
+    try:
+        with open("words.txt", "r") as file:
+            return file.read().splitlines()
+    except IOError:
+        print("Error: Could not load words.txt")
+        return []
 
-        return # exit function
-    if handCopy != handOrig:
-        print("FAILURE: test_update_hand('"+ word +"', " + str(handOrig) + ")")
-        print("\tOriginal hand was", handOrig)
-        print("\tbut implementation of update_hand mutated the original hand!")
-        print("\tNow the hand looks like this:", handCopy)
-        
-        return # exit function
-        
-    # test 2
-    handOrig = {'e':1, 'v':2, 'n':1, 'i':1, 'l':2}
-    handCopy = handOrig.copy()
-    word = "Evil"
+# Function to deal a new hand
+def deal_hand():
+    hand = []
+    for _ in range(HAND_SIZE):
+        letter = random.choice(list(SCRABBLE_LETTER_VALUES.keys()))
+        hand.append(letter)
+    return hand
 
-    hand2 = update_hand(handCopy, word)
-    expected_hand1 = {'v':1, 'n':1, 'l':1}
-    expected_hand2 = {'e':0, 'v':1, 'n':1, 'i':0, 'l':1}
-    if hand2 != expected_hand1 and hand2 != expected_hand2:
-        print("FAILURE: test_update_hand('"+ word +"', " + str(handOrig) + ")")        
-        print("\tReturned: ", hand2, "\n\t-- but expected:", expected_hand1, "or", expected_hand2)
+# Function to calculate the score of a word
+def calculate_word_score(word, hand_size):
+    word_length = len(word)
+    word_score = sum(SCRABBLE_LETTER_VALUES[letter] for letter in word)
+    second_component = 7 * word_length - 3 * (hand_size - word_length)
+    if second_component < 1:
+        second_component = 1
+    return word_score * second_component
 
-        return # exit function
+# Function to check if a word is valid
+def is_valid_word(word, hand, word_list):
+    word_freq = {}
+    for letter in hand:
+        word_freq[letter] = word_freq.get(letter, 0) + 1
 
-    if handCopy != handOrig:
-        print("FAILURE: test_update_hand('"+ word +"', " + str(handOrig) + ")")
-        print("\tOriginal hand was", handOrig)
-        print("\tbut implementation of update_hand mutated the original hand!")
-        print("\tNow the hand looks like this:", handCopy)
-        
-        return # exit function
+    for letter in word:
+        if word_freq.get(letter, 0) == 0:
+            return False
+        word_freq[letter] -= 1
 
-    # test 3
-    handOrig = {'h': 1, 'e': 1, 'l': 2, 'o': 1}
-    handCopy = handOrig.copy()
-    word = "HELLO"
+    return word.lower() in word_list
 
-    hand2 = update_hand(handCopy, word)
-    expected_hand1 = {}
-    expected_hand2 = {'h': 0, 'e': 0, 'l': 0, 'o': 0}
-    if hand2 != expected_hand1 and hand2 != expected_hand2:
-        print("FAILURE: test_update_hand('"+ word +"', " + str(handOrig) + ")")                
-        print("\tReturned: ", hand2, "\n\t-- but expected:", expected_hand1, "or", expected_hand2)
-        
-        return # exit function
+# Function to play the game
+def play_game():
+    word_list = load_words()
+    if not word_list:
+        return
 
-    if handCopy != handOrig:
-        print("FAILURE: test_update_hand('"+ word +"', " + str(handOrig) + ")")
-        print("\tOriginal hand was", handOrig)
-        print("\tbut implementation of update_hand mutated the original hand!")
-        print("\tNow the hand looks like this:", handCopy)
-        
-        return # exit function
+    hand = deal_hand()
+    print("Current hand:", " ".join(hand))
 
-    print("SUCCESS: test_update_hand()")
+    total_score = 0
+    while True:
+        word = input("Enter word or '.' to indicate you're finished: ")
+        if word == '.':
+            break
 
-# end of test_update_hand
+        if not is_valid_word(word, hand, word_list):
+            print("Invalid word. Try again.")
+            continue
 
-def test_is_valid_word(word_list):
-    """
-    Unit test for is_valid_word
-    """
-    failure=False
-    # test 1
-    word = "hello"
-    handOrig = get_frequency_dict(word)
-    handCopy = handOrig.copy()
+        word_score = calculate_word_score(word, len(hand))
+        total_score += word_score
+        print(f"'{word}' earned {word_score} points. Total score: {total_score}")
+        print()
 
-    if not is_valid_word(word, handCopy, word_list):
-        print("FAILURE: test_is_valid_word()")
-        print("\tExpected True, but got False for word: '" + word + "' and hand:", handOrig)
+    print("Total score:", total_score)
 
-        failure = True
-
-    # Test a second time to see if word_list or hand has been modified
-    if not is_valid_word(word, handCopy, word_list):
-        print("FAILURE: test_is_valid_word()")
-
-        if handCopy != handOrig:
-            print("\tTesting word", word, "for a second time - be sure you're not modifying hand.")
-            print("\tAt this point, hand ought to be", handOrig, "but it is", handCopy)
-
-        else:
-            print("\tTesting word", word, "for a second time - have you modified word_list?")
-            wordInWL = word in word_list
-            print("The word", word, "should be in word_list - is it?", wordInWL)
-
-        print("\tExpected True, but got False for word: '" + word + "' and hand:", handCopy)
-
-        failure = True
-
-
-    # test 2
-    hand = {'r': 1, 'a': 3, 'p': 2, 'e': 1, 't': 1, 'u':1}
-    word = "Rapture"
-
-    if  is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word()")
-        print("\tExpected False, but got True for word: '" + word + "' and hand:", hand)
-
-        failure = True        
-
-    # test 3
-    hand = {'n': 1, 'h': 1, 'o': 1, 'y': 1, 'd':1, 'w':1, 'e': 2}
-    word = "honey"
-
-    if  not is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word()")
-        print("\tExpected True, but got False for word: '"+ word +"' and hand:", hand)
-
-        failure = True                        
-
-    # test 4
-    hand = {'r': 1, 'a': 3, 'p': 2, 't': 1, 'u':2}
-    word = "honey"
-
-    if  is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word()")
-        print("\tExpected False, but got True for word: '" + word + "' and hand:", hand)
-        
-        failure = True
-
-    # test 5
-    hand = {'e':1, 'v':2, 'n':1, 'i':1, 'l':2}
-    word = "EVIL"
-    
-    if  not is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word()")
-        print("\tExpected True, but got False for word: '" + word + "' and hand:", hand)
-        
-        failure = True
-        
-    # test 6
-    word = "Even"
-
-    if  is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word()")
-        print("\tExpected False, but got True for word: '" + word + "' and hand:", hand)
-        print("\t(If this is the only failure, make sure is_valid_word() isn't mutating its inputs)")        
-        
-        failure = True        
-
-    if not failure:
-        print("SUCCESS: test_is_valid_word()")
-
-# end of test_is_valid_word
-
-def test_wildcard(word_list):
-    """
-    Unit test for is_valid_word
-    """
-    failure=False
-
-    # test 1
-    hand = {'a': 1, 'r': 1, 'e': 1, 'j': 2, 'm': 1, '*': 1}
-    word = "e*m"
-
-    if is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word() with wildcards")
-        print("\tExpected False, but got True for word: '" + word + "' and hand:", hand)
-
-        failure = True
-
-    # test 2
-    hand = {'n': 1, 'h': 1, '*': 1, 'y': 1, 'd':1, 'w':1, 'e': 2}
-    word = "honey"
-
-    if is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word() with wildcards")
-        print("\tExpected False, but got True for word: '"+ word +"' and hand:", hand)
-
-        failure = True
-
-    # test 3
-    hand = {'n': 1, 'h': 1, '*': 1, 'y': 1, 'd':1, 'w':1, 'e': 2}
-    word = "h*ney"
-
-    if not is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word() with wildcards")
-        print("\tExpected True, but got False for word: '"+ word +"' and hand:", hand)
-
-        failure = True
-
-    # test 4
-    hand = {'c': 1, 'o': 1, '*': 1, 'w': 1, 's':1, 'z':1, 'y': 2}
-    word = "c*wz"
-
-    if is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word() with wildcards")
-        print("\tExpected False, but got True for word: '"+ word +"' and hand:", hand)
-
-        failure = True    
-
-    # dictionary of words and scores WITH wildcards
-    words = {("h*ney", 7):290, ("c*ws", 6):176, ("wa*ls", 7):203}
-    for (word, n) in words.keys():
-        score = get_word_score(word, n)
-        if score != words[(word, n)]:
-            print("FAILURE: test_get_word_score() with wildcards")
-            print("\tExpected", words[(word, n)], "points but got '" + \
-                  str(score) + "' for word '" + word + "', n=" + str(n))
-            failure=True      
-
-    if not failure:
-        print("SUCCESS: test_wildcard()")
-
-
-word_list = load_words()
-print("----------------------------------------------------------------------")
-print("Testing get_word_score...")
-test_get_word_score()
-print("----------------------------------------------------------------------")
-print("Testing update_hand...")
-test_update_hand()
-print("----------------------------------------------------------------------")
-print("Testing is_valid_word...")
-test_is_valid_word(word_list)
-print("----------------------------------------------------------------------")
-print("Testing wildcards...")
-test_wildcard(word_list)
-print("All done!")
-
+# Main function to start the game
+if __name__ == "__main__":
+    print("Loading word list from file...")
+    words = load_words()
+    print(len(words), "words loaded.")
+    play_game()
